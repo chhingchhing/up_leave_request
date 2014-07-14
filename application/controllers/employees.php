c8cfebcea941d6d48c67ea26fb589619d1b53385
chhingchhing <chhingchhing.hem@gmail.com>
2014-07-14 20:50:25 +0700

    Remove die string

--- application/controllers/employees.php
+++ application/controllers/employees.php
@@ -96,117 +96,16 @@ class Employees extends Security
     function save($user_id=-1)
     {
 
-
-if ($this->input->post('upload_profile')) {
-    echo "upload_profile"; die();
-} else {
-    echo "string"; die();
-}
-
-        //upload image
-        $config = array(
-            'upload_path' => 'user_uploads/thumbnail/original/',
-            'allowed_types' => 'gif|jpg|png|GIF|JPG|PNG',
-            'max_size' => '20000'
-        ); 
-        $this->load->library('upload', $config);   
-        $configThumb = array(
-            'image_library' => 'gd2',
-            'upload_path' => 'user_uploads/thumbnail/thumb/',
-            'source_image' => '',
-            'create_thumb' => TRUE,
-            'maintain_ratio' => TRUE
+        $user_profile_data = array(
+            'first_name'=>$this->input->post('first_name'),
+            'last_name'=>$this->input->post('last_name'),
+            'email'=>$this->input->post('email'),
+            'gender'=>$this->input->post('gender'),
+            'address'=>$this->input->post('address'),
+            'phone1'=>$this->input->post('phone1'),
+            'phone2'=>$this->input->post('phone2'),
+            'dob'=>$this->input->post('dob')
         );
-        $this->load->library('image_lib');
-        $files = $_FILES;
-        $cpt = count($_FILES['upload_profile']['name']); 
-        // for ($i = 0; $i < $cpt; $i++) {
-            $_FILES['upload_profile']['name'] = str_replace(" ", "_", $files['upload_profile']['name']);
-            $_FILES['upload_profile']['name'] = str_replace("-", "_", $files['upload_profile']['name']);
-            //$_FILES['txt_photo']['name'] = $files['txt_photo']['name'];
-            $_FILES['upload_profile']['tmp_name'] = $files['upload_profile']['tmp_name'];
-            $_FILES['upload_profile']['type'] = $files['upload_profile']['type'];
-            $_FILES['upload_profile']['size'] = $files['upload_profile']['size'];     
-            if ($this->upload->do_upload('upload_profile')) {
-
-                if (file_exists ($config['upload_path']. $_FILES['upload_profile']['name'] )) {
-                    $image = $this->upload->data();
-                    $image = $_FILES['upload_profile']['name'];
-                    $data = $this->upload->data();
-                    if($data['is_image'] == 1) {
-                        //Replace string URL of full_path upload
-                        $data['full_path'] = str_replace('original', 'thumb', $data['full_path']);
-                        $configThumb['source_image'] = $data['full_path'];
-                       // var_dump( $configThumb['source_image']); die();
-                        $this->image_lib->initialize($configThumb);
-                        $this->image_lib->resize();
-                        //Get last element of string
-                        $last_start = strrpos($data['full_path'], '/');
-                        if($last_start !== false) {
-                          $last_field = substr($data['full_path'], $last_start);
-                        } else {
-                          $last_field = $data['full_path'];
-                        }
-                        // Get the name of gallery
-                        $image = ltrim ($last_field, '/');  //remove first character of string
-                    }
-                    if ($image != "") {
-                        $this->resize_image($config['upload_path'], $image, 150, 300);
-                    }
-                    // $image[$i] = $this->random(5).'-'.$image[$i];
-                } else {
-                   //  var_dump($config['upload_path']); die();
-                    $image = $this->upload->data();
-                    $image = $_FILES['upload_profile']['name'];
-                   // var_dump($image[$i]);
-                    if ($image != "") {
-                        $this->resize_image($config['upload_path'], $image, 150, 300);
-                    }
-                }
-            /* End of uploading */
-            } else {
-                $image = '';
-            }
-        // }
-  
-     // $result = $this->mod_gallery->add_galleries($image);
-    // var_dump($result); die();
-
-  /*if ($result>0) { //show message, already exist  
-  $this->session->set_userdata('create', show_message('Your data input was successfully.', 'success'));
-  //redirect('gallery/list_record');
-  redirect('gallery/view_galleries/'.$result);
-    }*/
-
-
-var_dump($image); die();
-
-
-        if ($image == "") {
-            $user_profile_data = array(
-                'first_name'=>$this->input->post('first_name'),
-                'last_name'=>$this->input->post('last_name'),
-                'email'=>$this->input->post('email'),
-                'gender'=>$this->input->post('gender'),
-                'address'=>$this->input->post('address'),
-                'phone1'=>$this->input->post('phone1'),
-                'phone2'=>$this->input->post('phone2'),
-                'dob'=>$this->input->post('dob')
-            );
-        } else {
-            $user_profile_data = array(
-                'first_name'=>$this->input->post('first_name'),
-                'last_name'=>$this->input->post('last_name'),
-                'email'=>$this->input->post('email'),
-                'gender'=>$this->input->post('gender'),
-                'address'=>$this->input->post('address'),
-                'phone1'=>$this->input->post('phone1'),
-                'phone2'=>$this->input->post('phone2'),
-                'dob'=>$this->input->post('dob'),
-                'photo' => $image
-            );
-        }
-        
         
         //Password has been changed OR first time password set
         if($this->input->post('password')!='')
